bb1503b2c8dc32d026949ccc87b9bcc865b17ce0
Klimanov <klimanov.dd@phystech.edu>
2021-12-10 14:52:24 +0300

    Added computing of phi functions

--- Pendulum_model.py
+++ Pendulum_model.py
@@ -12,7 +12,6 @@ def Euler_equations(dt, t):
     """
     x += dt*v
     alpha += dt*omega
-
     return x, alpha
 
 def RK_4(dt, t):
@@ -20,8 +19,10 @@ def RK_4(dt, t):
     Here we calculate the value of velocity and angle-velocity
     on the next iteration
     """
-    v += dt*(A*(np.sin(phi(t))*(phi_prime(t))**2 - np.cos(phi(t))*phi_dprime(t))
-    - b*v + omega**2*(x + A*np.sin(phi(t))) - g*np.cos(phi(t)) + k*(x - x0))
-    omega += dt*(g*np.sin(alpha) - omega*k*(x + A*sin(phi(t))) - 2*omega*(x + A*np.cos(phi(t))*phi_prime(t)))/(x + A*np.sin(phi(t)))
-
+    phi = A4*t**4 + B*t**3 + C*t**2 + D*t + E
+    phi_prime = 4*A4*t**3 + 3*B*t**2 + 2*C*t + D 
+    phi_dprime = 12*A4*t**2 + 6*B*t + 2*C
+    v += dt*(A*(np.sin(phi)*(phi_prime)**2 - np.cos(phi)*phi_dprime)
+    - b*v + omega**2*(x + A*np.sin(phi)) - g*np.cos(phi) + k*(x - x0))
+    omega += dt*(g*np.sin(alpha) - omega*k*(x + A*sin(phi)) - 2*omega*(x + A*np.cos(phi)*phi_prime))/(x + A*np.sin(phi))
     return v, omega
