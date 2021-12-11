cae4040760ba7a573ecf183f0a557b4075b02f81
Klimanov <klimanov.dd@phystech.edu>
2021-12-11 20:30:53 +0300

    pend.x added

--- Pendulum_model.py
+++ Pendulum_model.py
@@ -22,7 +22,7 @@ def RK_4(dt, t, pend):
     phi_prime = 4*pend.A4*t**3 + 3*pend.B*t**2 + 2*pend.C*t + pend.D 
     phi_dprime = 12*pend.A4*t**2 + 6*pend.B*t + 2*pend.C
     pend.v0 += dt*(pend.A*(np.sin(phi)*(phi_prime)**2 - np.cos(phi)*phi_dprime)
-    - pend.mu_m*pend.v + pend.w0**2*(pend.x0 + pend.A*np.sin(phi)) - g*np.cos(phi) + pend.k*(pend.x - x0))
+    - pend.mu_m*pend.v + pend.w0**2*(pend.x0 + pend.A*np.sin(phi)) - g*np.cos(phi) + pend.k*(pend.x - pend.x0))
     pend.w0 += dt*(g*np.sin(pend.alpha0) - pend.w0*pend.k*(pend.x + pend.A*np.sin(phi)) 
     - 2*pend.w0*(pend.x + pend.A*np.cos(phi)*phi_prime))/(pend.x + pend.A*np.sin(phi))
 
