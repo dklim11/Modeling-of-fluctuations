cfe9c6c894024bb32676cd6aa989547ee9ba9134
Malhux <krasnov.mikhail@phystech.edu>
2021-12-11 17:29:53 +0300

    Create module where class pendulum initialize

---
+++ Pendulum_objects.py
@@ -0,0 +1,14 @@
+class pendulum:
+    def __init__(self, x0, k, A, alpha0, v0, w0, mu_m, A4, B, C, D, E):
+        self.x0 = x0
+        self.k = k
+        self.A = A
+        self.alpha0 = alpha0
+        self.vo = v0
+        self.wo = w0
+        self.mu_m = mu_m
+        self.A4 = A4
+        self.B = B
+        self.C = C
+        self.D = D
+        self.E = E
