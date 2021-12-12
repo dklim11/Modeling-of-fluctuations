5569aba7feb725e3952610fac117d04bced91d78
Malhux <91050332+Malhux@users.noreply.github.com>
2021-12-12 15:31:01 +0300

    I added new atributes to class pendulum

--- Pendulum_objects.py
+++ Pendulum_objects.py
@@ -1,11 +1,13 @@
 class pendulum:
-    def __init__(self, x0, k, A, alpha0, v0, w0, mu_m, A4, B, C, D, E):
-        self.x0 = x0
+    def __init__(self, l0, l, k, A, s, alpha, v, w, mu_m, A4, B, C, D, E):
+        self.l0 = l0
+        self.l = l
         self.k = k
         self.A = A
-        self.alpha0 = alpha0
-        self.vo = v0
-        self.wo = w0
+        self.s = s
+        self.alpha = alpha
+        self.v = v
+        self.w = w
         self.mu_m = mu_m
         self.A4 = A4
         self.B = B
