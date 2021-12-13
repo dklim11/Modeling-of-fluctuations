62cf6124992536e23b59b6c0a66a06b91ffcb2ab
Klimanov <klimanov.dd@phystech.edu>
2021-12-13 11:07:00 +0300

    Added further documentation for module

--- Pendulum_model.py
+++ Pendulum_model.py
@@ -6,7 +6,7 @@ Here the gravitational constant
 g = 9.8
 dt = 0.001
 
-def Euler_equations(pend, dt, t):
+def Euler_equations(pend, t):
     """
     In this function we calculate values of x and alpha on the
     next step
@@ -16,10 +16,14 @@ def Euler_equations(pend, dt, t):
     pend.l += dt*pend.v + pend.A*cos(phi)*phi_prime*dt
     pend.alpha += dt*pend.w
 
-def RK_4(t, pend, dt):
+def RK_4(pend, t):
     """
     Here we calculate the value of velocity and angle-velocity
     on the next iteration
+    1) phi is a function of time in the argument of motor-coordinate sinus-function
+    2) phi_prime is the first prime of phi function of time
+    3) phi_dprime is the second prime of phi function of time
+    4) v - velocity; w - angle-velocity; aplha - angle; l - length of spring
     """
     phi = pend.A4*t**4 + pend.B*t**3 + pend.C*t**2 + pend.D*t + pend.E
     phi_prime = 4*pend.A4*t**3 + 3*pend.B*t**2 + 2*pend.C*t + pend.D 
@@ -28,4 +32,3 @@ def RK_4(t, pend, dt):
     - pend.mu_m*pend.v + pend.w0**2*(pend.l + pend.A*sin(phi)) - g*cos(phi) + pend.k*(pend.l - pend.l0))
     pend.w += dt*(g*sin(pend.alpha) - pend.w*pend.k*(pend.l + pend.A*sin(phi)) 
     - 2*pend.w*(pend.l + pend.A*cos(phi)*phi_prime))/(pend.l + pend.A*sin(phi))
-
