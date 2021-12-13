c73c99149600c604aebb1c54052741033c3dbc08
GavrichenkoKate <91049308+GavrichenkoKate@users.noreply.github.com>
2021-12-13 10:59:23 +0300

    almost everything is ready

--- Pendulum_main.py
+++ Pendulum_main.py
@@ -1,266 +1,53 @@
 import pygame
-import pendulum
-import time
+from pygame.draw import *
+import numpy as np
+pygame.init()
+
 from pendulum_vis import *
 from pendulum_model import *
 from pendulum_input import *
+from pendulum_objects import *
 
-
-time_step = None
-"""Шаг по времени при моделировании.
-Тип: float"""
 
 objects = []
 """Список объектов в системе."""
 
-def const_colors():
-    '''
-    Задаем основные цвета, используемые в программе.
-    Функция возвращает список из 7 цветов:
-    черный, красный, синий, желтый, зеленый, маджента, цвет морской волны
-    '''
-    
-    white = (255, 255, 255)
-    red = (255, 0, 0)
-    blue = (0, 0, 255)
-    yellow = (255, 255, 0)
-    green = (0, 255, 0)
-    magenta = (255, 0, 255)
-    cyan = (0, 255, 255)
-    black = (0, 0, 0)
-    colors = [white, red, blue, yellow, green, magenta, cyan, black]
-    return colors
-
-
-def execution():
-    """Функция исполнения -- выполняется циклически, вызывая обработку всех объектов,
-    а также обновляя их положение на экране.
-    Цикличность выполнения зависит от значения глобальной переменной perform_execution.
-    При perform_execution == True функция запрашивает вызов самой себя по таймеру через от 1 мс до 100 мс.
-    """
-    global physical_time
-    global displayed_time
-
-    if objects != []:
-        recalculate_objects_positions(objects, time_step.get()) 
-
-        """ из pendulum_model:
-        def recalculate_objects_positions(objects, dt):
-     Пересчитывает координаты объектов objects.
-     Параметры:
-     **objects** — список объектов, для которых нужно пересчитать координаты.
-     **dt** — шаг по времени"""
-
-        save_objects_positions(objects, physical_time)
-
-        """из pendulum_input:
-        def save_objects_positions(objects, time):
-    
-     Сохраняет данные о положении тел в файл phys_model.txt   в формате
-        <Физическое время с начала симуляции в секундах>
-        <...>
-        
-     Параметры:
-        **objects** — список объектов планет и звёзд
-        **physical_time** — время с начала симуляции
-        """
-    for body in pendulum_objects:
-        update_object_position(space, body)
-        """ из vis:
-        def update_object_position(space, body):
-     Перемещает отображаемый объект на холсте.
-     Параметры:
-     **space** — холст для рисования.
-     **body** — тело, которое нужно переместить.
-     x = scale_x(body.x)
-     y = scale_y(body.y)
-        """
-    physical_time += time_step.get()
-    displayed_time.set("%.1f" % physical_time + " seconds gone")
-
-    if perform_execution:
-        space.after(101 - int(time_speed.get()), execution)
-
-
-def start_execution():
-    """Обработчик события нажатия на кнопку Start.
-    Запускает циклическое исполнение функции execution.
-    """
-    global perform_execution
-    perform_execution = True
-    start_button['text'] = "Pause"
-    start_button['command'] = stop_execution
-
-    clear_stats(objects)
-
-    """из pendulum_input:
-    def clear_stats(objects):
-    
-    Очищает файл phys_model.txt
-    Сохраняет информацию о типах существующих тел в формате:
-        <...>
-    
-        #End of the header
-        
-     Параметры:
-        **objects** — список физических объектов в системе
-        **physical_time** — время с начала симуляции
-    """
-    execution()
-    print('Started execution...')
-
-
-def stop_execution():
-    """Обработчик события нажатия на кнопку Start.
-    Останавливает циклическое исполнение функции execution.
-    """
-    global perform_execution
-    perform_execution = False
-    start_button['text'] = "Start"
-    start_button['command'] = start_execution
-    print('Paused execution.')
-
- """def open_diagram():
-    Запускает считывание информации и отображает графики
-    stop_execution()
-    legend, t, r, v = get_data_for_plots()
-    draw_plots(legend, t, r, v)
- """
-
-def open_file_dialog():
-    """Открывает диалоговое окно выбора имени файла и вызывает
-    функцию считывания параметров системы физических объектов из данного файла.
-    Считанные объекты сохраняются в глобальный список objects
-    """
-    global objects
-    global perform_execution
-    global physical_time
-    stop_execution()
-    for obj in objects:
-        space.delete(obj.image)  # удаление старых изображений физ. тел
-    in_filename = askopenfilename(filetypes=(("Text file", ".txt"),))
-    if in_filename != '':
-        objects = read_parameters_of_pendulum_from_file(input_filename)
-        """из input:
-        def read_objects_data_from_file(input_filename):
-     Cчитывает данные о физических объектах из файла, создаёт сами объекты
-     и вызывает создание их графических образов
-     Параметры:
-     **input_filename** — имя входного файла
-        """
-        max_distance = max([max(abs(obj.x), abs(obj.y)) for obj in objects])
-        calculate_scale_factor(max_distance)
-
-        """из pendulum_vis:
-        def calculate_scale_factor(max_distance):
-     Вычисляет значение глобальной переменной **scale_factor** по данной характерной длине
-     global scale_factor
-     scale_factor = 0.4*min(window_height, window_width)/max_distance
-     print('Scale factor:', scale_factor)"""
-
-        for obj in pendulum_objects:
-            if obj.type == 'ball':
-                create_ball_image(space, obj)
-            if obj.type == 'kernel':
-                create_kernel_image(space,obj)
-            if   obj.type == 'spring':
-                create_spring_image(space,obj) 
-            if   obj.type == 'ceiling':
-                create_ceiling_image(space,obj) 
-            if   obj.type == 'point':
-                create_point_image(space,obj)
-            else:
-                raise AssertionError()
-        physical_time = 0
-
-
-def save_file_dialog():
-    """Открывает диалоговое окно выбора имени файла и вызывает
-    функцию считывания параметров системы небесных тел из данного файла.
-    Считанные объекты сохраняются в глобальный список space_objects
-    """
-    stop_execution()
-    out_filename = asksaveasfilename(filetypes=(("Text file", ".txt"),))
-    write_objects_data_to_file(out_filename, objects)
-
-    """из pendulum_input:
-    def write_space_objects_data_to_file(output_filename, space_objects):
-    Сохраняет данные о космических объектах в файл.
-    Строки должны иметь следующий формат:
-    <...> 
-    l0 - length of pendulum
-    k - asperity of spring
-    fi - function of time
-    A - amplitude of motor fluctuations
-    alpha0 - the start-up angle
-    Параметры:
-    **output_filename** — имя входного файла
-    **objects** — список физ объектов
-    """
-
-def menu(score):
-    '''
-    Заполняет информационное поле -- зону меню
-    :param score: количество очков
-    '''
-    text('length of pendulum: x0, asperity of spring: k, amplitude of motor fluctuations: A', (min(SIZE)*0.005, min(SIZE)*0.005))
-    text(str(pend.x0), (min(SIZE)*0.0005 + 120, min(SIZE)*0.0005))
-    text(str(pend.k), (min(SIZE)*0.0005 + 120, min(SIZE)*0.0005))
-    text(str(pend.A), (min(SIZE)*0.0005 + 120, min(SIZE)*0.0005))
-    
-
-def text(string, coords):
-    '''
-    Выводит заданный текст в заданном месте экрана
-    :param string: строка, которую выводим
-    :param coords: кортеж координат начала строки
-    '''
-    text1 = pygame.font.Font(None, 38)
-    text = text1.render(string, True, COLORS[-1])
-    screen.blit(text, coords)
-
 def main():
-    """Главная функция главного модуля.
-    Создаёт объекты графического дизайна библиотеки tkinter: окно, холст, фрейм с кнопками, кнопки.
-    """
-  """  global physical_time
-    global displayed_time
-    global time_step
-    global time_speed
-    global space
-    global start_button
-    print('Modelling started!')
-    physical_time = 0
-    root = tkinter.Tk()
-    root.title("Kepler's laws of motion")
-    root.geometry(window_size)
-    # пространство отображается на холсте типа Canvas
-    space = tkinter.Canvas(root, width= window_width, height = window_height, bg="white")
-    space.pack(side=tkinter.TOP)
-    # нижняя панель с кнопками
-    frame = tkinter.Frame(root)
-    frame.pack(side=tkinter.BOTTOM)
-    start_button = tkinter.Button(frame, text="Start", command=start_execution, width=6)
-    start_button.pack(side=tkinter.LEFT)
-    time_step = tkinter.DoubleVar()
-    time_step.set(1)
-    time_step_entry = tkinter.Entry(frame, textvariable=time_step)
-    time_step_entry.pack(side=tkinter.LEFT)
-    time_speed = tkinter.DoubleVar()
-    scale = tkinter.Scale(frame, variable=time_speed, orient=tkinter.HORIZONTAL)
-    scale.pack(side=tkinter.LEFT)
-    load_file_button = tkinter.Button(frame, text="Open file...", command=open_file_dialog)
-    load_file_button.pack(side=tkinter.LEFT)
-    save_file_button = tkinter.Button(frame, text="Save to file...", command=save_file_dialog)
-    save_file_button.pack(side=tkinter.LEFT)
-     open_diagram_button = tkinter.Button(frame, text="View stats", command=open_diagram)
-    open_diagram_button.pack(side=tkinter.LEFT) 
-    displayed_time = tkinter.StringVar()
-    displayed_time.set(str(physical_time) + " seconds gone")
-    time_label = tkinter.Label(frame, textvariable=displayed_time, width=30)
-    time_label.pack(side=tkinter.RIGHT)
-    root.mainloop()
-    print('Modelling finished!') """
+    FPS = 30
+    
+
+    ended = False
+    finished = False
+
+    screen_width = 800
+    screen_height = 800                   
+    screen = pygame.display.set_mode((screen_width, screen_height))
+
+    blue = (0, 184, 217)
+    grey = (133, 150, 150)
+    white = (255, 255, 255)
+    green = (153, 255, 153)
+    dark_green = (80, 200, 120)
+    yellow = (255, 255, 0)
+    purple = (128, 0, 255)
+    black = (0, 0, 0)
+
+
+
+if not started_already:
+            screen.fill(black)
+            clicked = False
+
+while not finished:
+    clicked.tick(dt) (t += dt)
+    {vis}
+    Euler_equations(pend, t, dt)
+    RK_4(pend, t, dt)
+     pygame.display.update()
+
+pygame.quit()
 
 if __name__ == "__main__":
     main()
+
+
