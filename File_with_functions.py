2b251bbd2f19edc085e4b4ee155ac5615818fa72
Klimanov <klimanov.dd@phystech.edu>
2021-12-17 09:18:46 +0300

    Equations are nearly correct

--- File_with_functions.py
+++ File_with_functions.py
@@ -1,6 +1,6 @@
 import pygame
 from pygame.draw import *
-from numpy import cos, sin, pi
+from numpy import cos, sin, pi, abs
 
 pygame.init()
 
@@ -135,8 +135,11 @@ def Euler_equations(pend, t, dt):
     """
     phi = pend.A4*t**4 + pend.B*t**3 + pend.C*t**2 + pend.D*t + pend.E
     phi_prime = 4*pend.A4*t**3 + 3*pend.B*t**2 + 2*pend.C*t + pend.D
-    pend.s = pend.A*sin(phi)
+    pend.s = pend.A*abs(sin(phi))
     pend.l += dt*pend.v 
+    if pend.l <= 0:
+        pend.l = -pend.l
+        pend.v = -pend.v
     pend.alpha += dt*pend.w
 
 def RK_4(pend, t, dt):
@@ -152,7 +155,7 @@ def RK_4(pend, t, dt):
     phi_prime = 4*pend.A4*t**3 + 3*pend.B*t**2 + 2*pend.C*t + pend.D 
     phi_dprime = 12*pend.A4*t**2 + 6*pend.B*t + 2*pend.C
     pend.v += dt*(-pend.mu_m*pend.v+(phi_prime**2)*(pend.l+pend.A*sin(phi))+g*cos(pend.alpha)-pend.k*(pend.l - pend.l0)-pend.A*phi_dprime*cos(phi)+pend.A*(phi_prime**2)*sin(phi))
-    pend.w += dt*(pend.mu_m*pend.w - (g*sin(pend.alpha)+2*(pend.v + pend.A*cos(phi)*phi_prime))/(pend.l + pend.A*sin(phi)))
+    pend.w -= dt*(pend.mu_m*pend.w + (g*sin(pend.alpha) + 2*(pend.v + pend.A*cos(phi)*phi_prime))/(pend.l + pend.A*sin(phi)))
 
 clock = pygame.time.Clock()
 finished = False
